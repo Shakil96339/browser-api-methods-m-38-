6ee5baf19b66d22b180c2b4c66551a3959dfa798
unknown <sh393035@gmail.com>
2021-09-07 06:11:20 +0600

    comitted

---
+++ alert.js
@@ -0,0 +1,12 @@
+const maComing = () => {
+	alert("ma is coming..open the book");
+};
+const askPicnic = () => {
+	const res = confirm("are you going to picnic");
+	// console.log(res);
+	if (res === true) {
+		alert("Bkash  e Taka patha");
+	} else {
+		alert("dmg!!");
+	}
+};
---
+++ hi.js
@@ -0,0 +1,10 @@
+const get = () => {
+	const quary = confirm("are you single");
+
+	if (quary === true) {
+		alert(`this is my phone number
+        72834712847`);
+	} else {
+		alert("valo thakis boin.");
+	}
+};
